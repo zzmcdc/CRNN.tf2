f2756cf0e418f4cea5b577eff7fae4e3ab83496b
zhao <zzmcdc@gmail.com>
2020-07-13 11:05:57 +0800

    add train

---
+++ train.sh
@@ -0,0 +1,4 @@
+python3 train.py --name crnn_lstm --train_lst /data/recog/reg_imgs/train.txt --val_lst /data/recog/reg_imgs/val.txt
+
+python train.py -ta TRAIN_ANNOTATION_PATHS -va VAL_ANNOTATION_PATHS -t TABLE_PATH
+
