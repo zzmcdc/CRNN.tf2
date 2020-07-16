43e513040fb4b60013e7b17fea80ba8d3f859097
ZHAOZHIMING <zhaozhiming@kingsoft.com>
2020-07-16 14:30:17 +0800

    fix code

--- train.py
+++ train.py
@@ -44,43 +44,6 @@ with open(args.charset) as to_read:
     classes = list(to_read.read().strip())
 num_classes = len(classes)
 
-
-class WarmUpLearningRateScheduler(keras.callbacks.Callback):
-    """Warmup learning rate scheduler
-    """
-
-    def __init__(self, warmup_batches, init_lr, verbose=0):
-        """Constructor for warmup learning rate scheduler
-
-        Arguments:
-            warmup_batches {int} -- Number of batch for warmup.
-            init_lr {float} -- Learning rate after warmup.
-
-        Keyword Arguments:
-            verbose {int} -- 0: quiet, 1: update messages. (default: {0})
-        """
-
-        super(WarmUpLearningRateScheduler, self).__init__()
-        self.warmup_batches = warmup_batches
-        self.init_lr = init_lr
-        self.verbose = verbose
-        self.batch_count = 0
-        self.learning_rates = []
-
-    def on_batch_end(self, batch, logs=None):
-        self.batch_count = self.batch_count + 1
-        lr = K.get_value(self.model.optimizer.lr)
-        self.learning_rates.append(lr)
-
-    def on_batch_begin(self, batch, logs=None):
-        if self.batch_count <= self.warmup_batches:
-            lr = self.batch_count*self.init_lr/self.warmup_batches
-            K.set_value(self.model.optimizer.lr, lr)
-            if self.verbose > 0:
-                print('\nBatch %05d: WarmUpLearningRateScheduler setting learning '
-                      'rate to %s.' % (self.batch_count + 1, lr))
-
-
 dataset_builder = DatasetBuilder(
     args.charset, args.img_width, args.img_channels, args.ignore_case)
 train_ds, train_size = dataset_builder.build(
@@ -100,7 +63,7 @@ saved_model_path = ('saved_models/{}/'.format(localtime) +
 os.makedirs('saved_models/{}'.format(localtime))
 print('Training start at {}'.format(localtime))
 
-model = build_model(num_classes, channels=args.img_channels)
+model = build_model(DatasetBuilder.num_classes, channels=args.img_channels)
 
 #model = multi_gpu_model(model, gpus=4)
 
