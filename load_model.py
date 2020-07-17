ced503155cd96601856240dcfa3bd886296c4310
ZHAOZHIMING <zhaozhiming@kingsoft.com>
2020-07-17 10:47:55 +0800

    fix code

--- load_model.py
+++ load_model.py
@@ -1,13 +1,13 @@
 import os
+from tensorflow import keras
 from model import build_model
-restore = '015_0.4931_0.3950.h5'
+restore = './008_0.3520_0.3214.h5'
 
 with open("./label.txt") as to_read:
     classes = list(to_read.read().strip())
 
-num_classes = len(classes) +1 
-print(num_classes)
-model = build_model(num_classes,256, 1)
+num_classes = len(classes) + 1
+model = build_model(num_classes, 256, 1)
 model.load_weights(restore, by_name=True, skip_mismatch=True)
-print(model.summary())
+# model.summary()
 model.save("crnn_tf")
