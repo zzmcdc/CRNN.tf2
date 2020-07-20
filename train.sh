cd427304e50804bcd3f7bb5fa391b8af28345662
zhao <zzmcdc@gmail.com>
2020-07-20 15:06:02 +0800

    lr

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
-python3 train.py -ta /data/recog/reg_imgs/train.txt  -va /data/recog/reg_imgs/val.txt --charset label_tf.txt --img_width 256  --epochs 120 -lr 0.001
+python3 train.py -ta /data/recog/reg_imgs/train.txt  -va /data/recog/reg_imgs/val.txt --charset label_tf.txt --img_width 256  --epochs 120 -lr 0.01
 
 
