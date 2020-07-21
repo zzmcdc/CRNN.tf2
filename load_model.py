390160c9fb471b2d3fad33485876cfb1fff69194
ZHAOZHIMING <zhaozhiming@kingsoft.com>
2020-07-21 11:20:39 +0800

    fix

--- load_model.py
+++ load_model.py
@@ -1,13 +1,13 @@
 import os
 from tensorflow import keras
 from model import build_model
-restore = './026_0.7094_0.3820.h5'
+restore = './034_0.5839_0.4369.h5'
 
 with open("./label.txt") as to_read:
     classes = list(to_read.read().strip())
 
 num_classes = len(classes) + 1
-model = build_model(num_classes, 256, 1)
+model = build_model(num_classes, 256, 1,True)
 model.load_weights(restore, by_name=True, skip_mismatch=True)
 # model.summary()
 model.save("crnn_tf")
--- model.py
+++ model.py
@@ -8,7 +8,7 @@ def vgg_style(input_tensor):
     The original feature extraction structure from CRNN paper.
     Related paper: https://ieeexplore.ieee.org/abstract/document/7801919
     """
-    x = layers.Conv2D(64, 3, padding='same',use_bias=False)(input_tensor)
+    x = layers.Conv2D(64, 3, padding='same', use_bias=False)(input_tensor)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
     x = layers.MaxPool2D(pool_size=2, padding='same')(x)
@@ -32,8 +32,8 @@ def vgg_style(input_tensor):
     x = layers.Conv2D(512, 3, padding='same', use_bias=False)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
-    
-    x = layers.Conv2D(1024, 3, padding='same')(x)
+
+    x = layers.Conv2D(512, 3, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
 
@@ -46,12 +46,16 @@ def vgg_style(input_tensor):
     return x
 
 
-def build_model(num_classes, image_width=None, channels=1):
+def build_model(num_classes, image_width=None, channels=1, training=True):
     """build CNN-RNN model"""
 
     img_input = keras.Input(shape=(32, image_width, channels))
     x = vgg_style(img_input)
-    x = layers.Reshape((63, 1024))(x)
+    if training:
+        x = layers.Reshape((63, 1024))(x)
+    else:
+        x = x[0, 0, :, :]
 
-    x = layers.Dense(units=num_classes,kernel_regularizer=regularizers.l2(1e-4))(x)
+    x = layers.Dense(units=num_classes,
+                     kernel_regularizer=regularizers.l2(1e-4))(x)
     return keras.Model(inputs=img_input, outputs=x, name='CRNN')
