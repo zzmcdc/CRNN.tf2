50f9feedf5213ee864c8779cefb0469141b6f780
zhao <zzmcdc@gmail.com>
2020-07-21 08:28:06 +0800

    fix

--- model.py
+++ model.py
@@ -8,22 +8,35 @@ def vgg_style(input_tensor):
     The original feature extraction structure from CRNN paper.
     Related paper: https://ieeexplore.ieee.org/abstract/document/7801919
     """
-    x = layers.Conv2D(64, 3, padding='same', activation='relu')(input_tensor)
+    x = layers.Conv2D(64, 3, padding='same',use_bias=False)(input_tensor)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
     x = layers.MaxPool2D(pool_size=2, padding='same')(x)
 
-    x = layers.Conv2D(128, 3, padding='same', activation='relu')(x)
+    x = layers.Conv2D(128, 3, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+
     x = layers.MaxPool2D(pool_size=2, padding='same')(x)
 
     x = layers.Conv2D(256, 3, padding='same', use_bias=False)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
-    x = layers.Conv2D(256, 3, padding='same', activation='relu')(x)
+
+    x = layers.Conv2D(256, 3, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+
     x = layers.MaxPool2D(pool_size=2, strides=(2, 1), padding='same')(x)
 
     x = layers.Conv2D(512, 3, padding='same', use_bias=False)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation('relu')(x)
-    x = layers.Conv2D(1024, 3, padding='same', activation='relu')(x)
+    
+    x = layers.Conv2D(1024, 3, padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+
     x = layers.MaxPool2D(pool_size=2, strides=(2, 1), padding='same')(x)
 
     x = layers.Conv2D(1024, 2, use_bias=False)(x)
